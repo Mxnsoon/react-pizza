df1fca9514a3568f1dcb29cd054927a79408a62d
Mxnsoon <Nigga.guy@yandex.ru>
2022-06-15 17:26:16 +0300

    skeleton

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './scss/app.scss';
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
 
-import pizzas from './assets/pizzas.json';
-
-console.log(pizzas)
+import {useEffect, useState} from "react";
+import Skeleton from "./components/PizzaBlock/Skeleton";
 
 function App() {
+    const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetch('https://62a9d80d3b314385543cca25.mockapi.io/items').then(res => res.json())
+            .then((arr) => {
+                setItems(arr);
+                setIsLoading(false)
+            })
+    }, [])
+
     return (
         <div className="wrapper">
             <Header/>
@@ -21,10 +31,11 @@ function App() {
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
                         {
-                            pizzas.map(obj => (
-                                <PizzaBlock key={obj.id} {...obj}
-                                />
-                            ))
+                            isLoading
+                                ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+                                : items.map(obj => (
+                                    <PizzaBlock key={obj.id} {...obj} />
+                                ))
                         }
                     </div>
                 </div>
---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const Skeleton = (props) => (
+    <ContentLoader
+        className="pizza-block"
+        speed={2}
+        width={280}
+        height={465}
+        viewBox="0 0 280 465"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+        {...props}
+    >
+        <circle cx="134" cy="136" r="125" />
+        <rect x="0" y="288" rx="10" ry="10" width="280" height="18" />
+        <rect x="0" y="318" rx="0" ry="0" width="280" height="88" />
+        <rect x="0" y="421" rx="10" ry="10" width="95" height="30" />
+        <rect x="51" y="441" rx="0" ry="0" width="5" height="6" />
+        <rect x="131" y="412" rx="25" ry="25" width="152" height="45" />
+        <rect x="213" y="431" rx="0" ry="0" width="1" height="1" />
+    </ContentLoader>
+)
+
+export default Skeleton
